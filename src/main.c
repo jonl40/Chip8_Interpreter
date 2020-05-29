8cfc0b70fb7a0b38455a657c16b92a38aedaddcc
Jonathan <jonathanluu122@gmail.com>
2020-05-28 23:49:20 -0400

    wip

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "SDL2/SDL.h"
 #include "chip8.h"
 #include "chip8memory.h"
 
@@ -22,9 +24,49 @@ int main(int argc, char *argv[])
     /* load rom */
 
     /* Emulation loop */
-    // execute opcode 
-    // draw to screen
-    // read keypresses 
+
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create SDL window 
+    SDL_Window *window = SDL_CreateWindow("Chip8 Interpreter", 
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, CHIP_8_SCREEN_WIDTH * CHIP_8_UPSCALER,
+    CHIP_8_SCREEN_HEIGHT * CHIP_8_UPSCALER, SDL_WINDOW_OPENGL);
+
+    // Create a renderere (accelerated and insync with the dispay refresh rate)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    
+    // Draw colour is white 
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+
+    bool running = true;
+    SDL_Event event;
+    while(running)
+    {
+        //process events
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+                running = false; 
+        }
+
+        // Draw 
+        SDL_Rect rect = {50, 100, 45, 45}; // x, y, width, height 
+        SDL_RenderFillRect(renderer, &rect);
+
+        // Display what you drew
+        SDL_RenderPresent(renderer);
+
+
+        // execute opcode 
+        // draw to screen
+        // read keypresses 
+    }
+
+    // destroy the rendering context for a window and free associated textures
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
